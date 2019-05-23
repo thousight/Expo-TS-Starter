47274fcdde72824bb5b2358754d619754713d037
Mark Wen <markwenguojie94@gmail.com>
2019-05-23 12:19:10 -0400

    initialize draggable switch testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Switch from 'react-native-draggable-switch'
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Happy Hacking!</Text>
-  </View>
-)
+class App extends React.Component {
+  state = {
+    isSwitchOn: false,
+  }
+
+  render() {
+    const { isSwitchOn } = this.state
+    return (
+      <View style={styles.container}>
+        <Text>Happy Hacking!</Text>
+        <Switch
+          width={50}
+          height={30}
+          value={this.state.isSwitchOn}
+          onValueChange={isSwitchOn => this.setState({ isSwitchOn })}
+        />
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
